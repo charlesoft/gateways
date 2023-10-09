5748861c5eb607ee7e78a33218be272c59e07cf4
Charles Furtado <charlees1509@hotmail.com>
2023-10-09 11:05:36 -0300

    Add seeds data.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,20 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Gateways.Repo.insert!(%Gateways.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Gateways.Repo
+alias Gateways.Vacation.Place
+
+%Place{
+  name: "Sand Castle",
+  location: "Portugal",
+  max_guests: 2
+} |> Repo.insert!()
+
+%Place{
+  name: "New York Hotel",
+  location: "United States",
+  max_guests: 6
+} |> Repo.insert!()
+
+%Place{
+  name: "London National Park",
+  location: "England",
+  max_guests: 8
+} |> Repo.insert!()
