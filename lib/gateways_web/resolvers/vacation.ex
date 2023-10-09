0cbeb3beecef65b8509aa37b3e9a7126f7e685f3
Charles Furtado <charlees1509@hotmail.com>
2023-10-09 16:39:36 -0300

    Create mutation for creating places.

--- lib/gateways_web/resolvers/vacation.ex
+++ lib/gateways_web/resolvers/vacation.ex
@@ -8,4 +8,8 @@ defmodule GatewaysWeb.Resolvers.Vacation do
   def place(_, %{id: id}, _) do
     {:ok, Vacation.get_place!(id)}
   end
+
+  def create_place(_, args, _) do
+    Vacation.create_place(args)
+  end
 end
--- lib/gateways_web/schema/schema.ex
+++ lib/gateways_web/schema/schema.ex
@@ -14,6 +14,17 @@ defmodule GatewaysWeb.Schema.Schema do
     end
   end
 
+  mutation do
+    @desc "Create a place"
+    field :create_place, type: :place do
+      arg :name, non_null(:string)
+      arg :location, non_null(:string)
+      arg :max_guests, non_null(:integer)
+
+      resolve(&GatewaysWeb.Resolvers.Vacation.create_place/3)
+    end
+  end
+
   object :place do
     field :id, non_null(:id)
     field :name, non_null(:string)
